67886e17308a435df64e17330f65455e0d2b3831
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-12 19:46:32 +0530

    Update dependencyInstall.groovy

--- vars/dependencyInstall.groovy
+++ vars/dependencyInstall.groovy
@@ -1,13 +1,13 @@
-
-def call(String depCheckVersion, String depdir ) {
+def call(String depCheckVersion, String depdir) {
     sh """
         DEP_CHECK_DIR="${depdir}"
 
-        if [ ! -d "$DEP_CHECK_DIR" ]; then
+        if [ ! -d "\${DEP_CHECK_DIR}" ]; then
+            echo "Directory does not exist, downloading DependencyCheck..."
             curl -sLO https://github.com/jeremylong/DependencyCheck/releases/download/v${depCheckVersion}/dependency-check-${depCheckVersion}-release.zip
             unzip dependency-check-${depCheckVersion}-release.zip
         fi
 
-        ${depdir}/bin/dependency-check.sh --version
+        \${DEP_CHECK_DIR}/bin/dependency-check.sh --version
     """
 }
