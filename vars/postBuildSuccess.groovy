2f886659a889eaa736c6d9e1ffc111e24a2ff67f
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-15 10:47:06 +0530

    Update postBuildSuccess.groovy

--- vars/postBuildSuccess.groovy
+++ vars/postBuildSuccess.groovy
@@ -5,7 +5,7 @@ def call(String jobName, String buildNumber, String buildUrl, String toEmail) {
         message: "Successful: Bug and report published on SonarQube Server. Job Details - Name: ${jobName}, Build Number: ${buildNumber}, URL: ${buildUrl}"
     )
     emailext(
-        attachmentsPattern: 'dependency_report.txt',
+        attachmentsPattern: 'reports/Bug-report.json',
         body: """
             Hello,
             
