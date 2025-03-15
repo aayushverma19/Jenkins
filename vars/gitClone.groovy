27a9c6ecb4e804d366db501f416ebfaa91b3be95
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-15 09:13:23 +0530

    Update gitClone.groovy

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,1 +1,6 @@
-
+def call(String branch, String repoUrl, String gitPassword ) {
+  checkout([$class: 'GitSCM',
+              branches: [[name: "*/${branch}"]],
+              userRemoteConfigs: [[url: repoUrl , credentialsId: gitPassword ]]
+           ])
+}
