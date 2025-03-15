ec39ecb2a6271e7802c708fab6ebc17976be6c8d
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-16 00:50:18 +0530

    Update postBuildSuccess.groovy

--- vars/postBuildSuccess.groovy
+++ vars/postBuildSuccess.groovy
@@ -5,7 +5,8 @@ def call(String jobName, String buildNumber, String buildUrl, String toEmail) {
         message: "Successful: Test case passed and report published. Job Details - Name: ${jobName}, Build Number: ${buildNumber}, URL: ${buildUrl}"
     )
     emailext(
-        attachmentsPattern: '**/target/**/*.xml',
+        attachmentsPattern: 'target/site/jacoco/**',
+        //attachmentsPattern: '**/target/**/*.xml',
         body: """
             Hello,
             
